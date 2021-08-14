d95fde9a70ef6868c0e20b4ed4bb6100531238dc
Sands-45 <sandsqa@hotmail.com>
2021-08-14 14:55:40 +0200

    Update

--- js/charts.js
+++ js/charts.js
@@ -1,19 +1,54 @@
 let options = {
-  chart: {
-    type: "line",
-  },
   series: [
     {
       name: "Sales",
-      data: [2568, 5285, 4872, 3689, 5863, 5263],
+      type: "area",
+      data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33],
     },
-	{
-      name: "Lose",
-      data: [256, 585, 1002, 859, 863, 630],
-    }
+    {
+      name: "Comparison",
+      type: "line",
+      data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43],
+    },
   ],
-  xaxis: {
-    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  chart: {
+    height: 350,
+    type: "line",
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  fill: {
+    type: "solid",
+    opacity: [0.35, 1],
+  },
+  labels: [
+    "Dec 01",
+    "Dec 02",
+    "Dec 03",
+    "Dec 04",
+    "Dec 05",
+    "Dec 06",
+    "Dec 07",
+    "Dec 08",
+    "Dec 09 ",
+    "Dec 10",
+    "Dec 11",
+  ],
+  markers: {
+    size: 0,
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    y: {
+      formatter: function (y) {
+        if (typeof y !== "undefined") {
+          return y.toFixed(0) + " points";
+        }
+        return y;
+      },
+    },
   },
   stroke: {
     curve: "smooth",
@@ -23,14 +58,6 @@ let options = {
       lines: {
         show: false,
       },
-    },
-  },
-  title: {
-    text: "Annual Revenue",
-    align: "center",
-    style: {
-      fontSize: "16px",
-      color: "#2d73ce",
     },
   },
   chart: {
@@ -43,3 +70,49 @@ let options = {
 let chart = new ApexCharts(document.querySelector("#chart"), options);
 
 chart.render();
+
+/* =======================Chart 2 ==================*/
+
+var options2 = {
+  series: [
+    {
+      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+    },
+  ],
+  chart: {
+    type: "bar",
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: true,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  xaxis: {
+    categories: [
+      "South Korea",
+      "Canada",
+      "United Kingdom",
+      "Netherlands",
+      "Italy",
+      "France",
+      "Japan",
+      "United States",
+      "China",
+      "Germany",
+    ],
+  },
+  grid: {
+    yaxis: {
+      lines: {
+        show: false,
+      },
+    },
+  },
+};
+
+var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+chart2.render();
