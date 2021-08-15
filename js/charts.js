88bc9f01a558ab7a8fd7728333c6e5a793736d97
Sands-45 <sandsqa@hotmail.com>
2021-08-15 16:12:09 +0200

    Update

--- js/charts.js
+++ js/charts.js
@@ -1,130 +1,5 @@
 //Dash One ===================
 
-/* =============== miniStat RadialBar ============ */
-let options1 = {
-  series: [76, 67, 61],
-  chart: {
-    type: "radialBar",
-  },
-  plotOptions: {
-    radialBar: {
-      offsetY: 0,
-      startAngle: 0,
-      endAngle: 270,
-      hollow: {
-        margin: 5,
-        size: "30%",
-        background: "transparent",
-        image: undefined,
-      },
-      dataLabels: {
-        name: {
-          show: false,
-        },
-        value: {
-          show: false,
-        },
-      },
-    },
-  },
-  colors: ["#0084ff", "#39539E", "#0077B5"],
-  labels: ["Online", "CallCenter", "Counter"],
-  legend: {
-    show: true,
-    floating: true,
-    fontSize: "8px",
-    position: "left",
-    offsetY: -10,
-    offsetX: -10,
-    labels: {
-      useSeriesColors: true,
-    },
-    markers: {
-      size: 0,
-    },
-    formatter: function (seriesName, opts) {
-      return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex];
-    },
-    itemMargin: {
-      vertical: 3,
-    },
-  },
-  responsive: [
-    {
-      breakpoint: 480,
-      options: {
-        legend: {
-          show: false,
-        },
-      },
-    },
-  ],
-  title: {
-    text: "Channel",
-    align: "right",
-    style: {
-      fontSize: "14px",
-      fontWeight: "bold",
-      fontFamily: "sans-serif",
-      color: "#bee3db",
-    },
-  },
-};
-const chart1 = new ApexCharts(document.querySelector("#chart1"), options1);
-chart1.render();
-
-/* =============== Polar chart Monochrome ============ */
-let options2 = {
-  series: [42, 47, 52],
-  chart: {
-    height: "10rem",
-    type: "polarArea",
-  },
-  labels: ["Steers", "Deb", "Fishawyas"],
-  fill: {
-    opacity: 1,
-  },
-  stroke: {
-    width: 1,
-    colors: undefined,
-  },
-  yaxis: {
-    show: false,
-  },
-  legend: {
-    position: "bottom",
-  },
-  plotOptions: {
-    polarArea: {
-      rings: {
-        strokeWidth: 0,
-      },
-      spokes: {
-        strokeWidth: 0,
-      },
-    },
-  },
-  theme: {
-    monochrome: {
-      enabled: true,
-      shadeTo: "light",
-      shadeIntensity: 0.6,
-    },
-  },
-  title: {
-    text: "Upsell By Brand",
-    align: "right",
-    style: {
-      fontSize: "14px",
-      fontWeight: "bold",
-      fontFamily: "sans-serif",
-      color: "#bee3db",
-    },
-  },
-};
-const chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
-chart2.render();
-
 /* ===============Chart One ===========*/
 let options3 = {
   series: [
@@ -191,7 +66,7 @@ let options3 = {
   },
   title: {
     text: "Sales By Year",
-    align: "right",
+    align: "center",
     style: {
       fontSize: "14px",
       fontWeight: "bold",
@@ -250,7 +125,7 @@ let options4 = {
   },
   title: {
     text: "Total Sales Per Country",
-    align: "right",
+    align: "center",
     style: {
       fontSize: "14px",
       fontWeight: "bold",
