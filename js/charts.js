d7620bdb07a655f9abbbf1d0c94c58d604ebe7d6
Sands-45 <sandsqa@hotmail.com>
2021-08-15 07:41:23 +0200

    Update

--- js/charts.js
+++ js/charts.js
@@ -1,4 +1,4 @@
-
+//Dash One ===================
 /* ===============Chart One ===========*/
 let options = {
   series: [
@@ -115,4 +115,52 @@ var options2 = {
 var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
 chart2.render();
 
-/* =============== Spark Line ============ */
+/* =============== miniStat RadialBar ============ */
+var options3 = {
+  series: [44, 55, 67, 83],
+  chart: {
+    type: "radialBar",
+  },
+  plotOptions: {
+    radialBar: {
+      dataLabels: {
+        name: {
+          fontSize: "15px",
+        },
+        value: {
+          fontSize: "11px",
+        },
+        total: {
+          show: true,
+          label: "Total",
+          formatter: function (w) {
+            // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+            return 249;
+          },
+        },
+      },
+    },
+  },
+  labels: ["Apples", "Oranges", "Bananas", "Berries"],
+};
+
+var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
+chart3.render();
+
+var options4 = {
+  series: [70],
+  chart: {
+    type: "radialBar",
+  },
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        size: "70%",
+      },
+    },
+  },
+  labels: ["Cricket"],
+};
+
+var chart4 = new ApexCharts(document.querySelector("#chart4"), options4);
+chart4.render();
