f965a20b109a97a234ad7ad78aadb52c5237d4e3
Sands-45 <sandsqa@hotmail.com>
2021-08-18 00:32:13 +0200

    Dash 2 Update

--- js/charts.js
+++ js/charts.js
@@ -233,6 +233,48 @@ var trend = {
     },
   },
 };
-
 var trend = new ApexCharts(document.querySelector("#trend"), trend);
 trend.render();
+
+ /*===================== Comparison Chart ==============*/
+ var monochrome = {
+   series: [42, 47, 52, 58, 65],
+   chart: {
+     width: 380,
+     type: "polarArea",
+   },
+   labels: ["Online", "Google", "Youtube", "Ads", "Twitter"],
+   fill: {
+     opacity: 1,
+   },
+   stroke: {
+     width: 1,
+     colors: undefined,
+   },
+   yaxis: {
+     show: false,
+   },
+   legend: {
+     position: "bottom",
+   },
+   plotOptions: {
+     polarArea: {
+       rings: {
+         strokeWidth: 0,
+       },
+       spokes: {
+         strokeWidth: 0,
+       },
+     },
+   },
+   theme: {
+     monochrome: {
+       enabled: true,
+       shadeTo: "light",
+       shadeIntensity: 0.6,
+     },
+   },
+ };
+
+ var monochrome = new ApexCharts(document.querySelector("#monochrome"), monochrome);
+ monochrome.render();
